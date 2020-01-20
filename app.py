3960229c7f2c0a67b5a17b8af9b2a90020cf5c72
David Newgas <david@newgas.net>
2020-01-20 15:33:57 -0800

    basic openapi support

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import os
 
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from main import handleHttp
 
 app = Flask(__name__)
@@ -11,6 +11,16 @@ app = Flask(__name__)
 @app.route('/roll', methods=["POST"])  # compatibility with cloud func
 def entry():
     return handleHttp(request)
+
+
+@app.route('/v1/openapi.yaml')
+def openapi():
+    return send_from_directory('static', 'openapi.yaml')
+
+
+@app.route('/v1/dialogflow.yaml')
+def dialogflow_openapi():
+    return send_from_directory('static', 'dialogflow.yaml')
 
 
 if __name__ == "__main__":
